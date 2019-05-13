0f7cd9590547766e3431496555c5b9f7fea8d87e
likunhong01 <42308753+likunhong01@users.noreply.github.com>
2019-05-13 13:40:01 +0800

    init and login

---
+++ app01msbm/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class App01MsbmConfig(AppConfig):
+    name = 'app01msbm'
---
+++ app01msbm/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import requests, json
+
+# Create your views here.
+
+def login(request):
+    if request.method == 'GET':
+        code = request.GET.get('code')
+        appid = 'wx6b7b3078d85781ae'
+        appsecret = 'be19c75c943ac1b4065066087826e890'
+        response = {}
+        if not code or not appid:
+            response['message'] = '缺少code'
+            # response['code'] = ReturnCode.BROKEN_AUTHORIZED_DATA
+            return JsonResponse(data=response, safe=False)
+
+        api_url = 'https://api.weixin.qq.com/sns/jscode2session?appid='+ appid \
+              + '&secret=' + appsecret + '&js_code=' + code + '&grant_type=authorization_code'
+        # api_response = requests.get(url=api_url, proxies=proxy.proxy())
+        api_response = requests.get(url=api_url)
+        openid = json.loads(api_response.text).get('openid')
+        print(openid)
+        response['openid'] = openid
+        return JsonResponse(data=response,safe=False)
+
